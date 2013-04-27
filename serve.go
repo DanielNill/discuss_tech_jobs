8eeefb0a67c044c91e078e1f0585ee06a33c4f7d
Daniel Nill <DanielLNill@gmail.com>
2013-04-27 09:46:14 -0700

    basic post creation

--- serve.go
+++ serve.go
@@ -6,47 +6,73 @@ import (
   "net/http"
   _ "github.com/lib/pq"
   "database/sql"
+  "code.google.com/p/go.crypto/bcrypt"
 )
-func DBConnect() (conn) {
+
+func OpenConnection() *sql.DB {
   conn, err := sql.Open("postgres", "user=Daniel password=*Mrbobn1 dbname=discuss_dev_jobs sslmode=disable")
   if err != nil {
     fmt.Println(err)
   }
-  return
+  return conn
 }
 
 func landing(w http.ResponseWriter, r *http.Request){
-  t, _ := template.ParseFiles("home.html")
-  t.Execute(w, nil)
-}
-
-func newPost(w http.ResponseWriter, r *http.Request){
-  t, _ := template.ParseFiles("new.html")
-  t.Execute(w, nil)
-}
-
-func create(w http.ResponseWriter, r *http.Request){
-  conn := DBConnect()
-  rows, err := conn.Query("SELECT * FROM names")
+  conn := OpenConnection()
+  defer conn.Close()
+  rows, err := conn.Query("SELECT * FROM posts p JOIN users u ON p.user_id = u.user_id LIMIT 100")
   if err != nil {
     fmt.Println(err)
-  } else{
+  } else {
+    //columns, _ := rows.Columns()
+    //values := make([]interface{}, len(columns))
+    //scanArgs := make([]interface{}, len(values))
+    //for i := range values {
+    //  scanArgs[i] = &values[i]
+    //}
     for rows.Next() {
-      var name_id int
-      var name string
-      err = rows.Scan(&name_id, &name)
-      fmt.Println(rows)
-      fmt.Println(name_id)
-      fmt.Println(name)
+      var post_title string
+      var user_name string
+      var post_id int
+      rows.Scan(&post_title, &user_name, &post_id)
     }
   }
   t, _ := template.ParseFiles("home.html")
   t.Execute(w, nil)
 }
 
+func newPostForm(w http.ResponseWriter, r *http.Request){
+  t, _ := template.ParseFiles("new_post.html")
+  t.Execute(w, nil)
+}
+
+func createNewPost(w http.ResponseWriter, r *http.Request){
+  conn := OpenConnection()
+  _, err := conn.Exec("INSERT INTO posts (title, user_id) VALUES ('" + r.FormValue("title") + "', 1)")
+  if err != nil {
+    fmt.Println(err)
+  }
+  t, _ := template.ParseFiles("home.html")
+  t.Execute(w, nil)
+}
+
+func newUserForm(w http.ResponseWriter, r *http.Request){
+  t, _ := template.ParseFiles("new_user.html")
+  t.Execute(w, nil)
+}
+
+func createNewUser(w http.ResponseWriter, r *http.Request){
+  conn := OpenConnection()
+  email := r.FormValue("email")
+  password, _ := bcrypt.GenerateFromPassword([]byte(r.FormValue("password")), 0)
+  conn.Exec("INSERT INTO users (email, password_hash) VALUES (?, ?)", email, password)
+}
+
 func main(){
   http.HandleFunc("/", landing)
-  http.HandleFunc("/new", newPost)
-  http.HandleFunc("/create", create)
+  http.HandleFunc("/new_post", newPostForm)
+  http.HandleFunc("/create_new_post", createNewPost)
+  http.HandleFunc("/new_user", newUserForm)
+  http.HandleFunc("/create_new_user", createNewUser)
   http.ListenAndServe(":3001", nil)
 }
